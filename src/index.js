31979046243de61cc3a8d5bdb51a573dc2ab3a61
Nilay Chikani <nilay.chikani@sterlingcheck.com>
2020-07-15 11:53:24 +0530

    aws init

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-console.log(config);
 Amplify.configure(config);
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
